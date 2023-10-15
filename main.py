3b7d96763b583f34fe38c9cd47608e7d04a2047d
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-10-15 18:20:55 +0300

    доработка основного кода вывода счетов и карт

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from utils import display_last_operations, checking_the_dictionary
 from datetime import date
 
@@ -7,20 +5,22 @@ if __name__ == '__main__':
     display_last = display_last_operations()
     checking_dictonary = checking_the_dictionary(display_last)
 
+    # Перебор данных и установка даты в правильном порядке
     for display in checking_dictonary:
         dates = display["date"][:10]
         date_format = date.fromisoformat(dates)
         print(f"{date_format.day}.{date_format.month}.{date_format.year} {display['description']}")
+
+        # Вывод счетов со скрытыми в виде * значениями
         where_from = display['to'].split(" ")
 
         try:
             where = display['from'].split(" ")
             if "from" not in where:
-                print(f"{where_from[0]} {where_from[1][:4]} {where_from[1][4:6]}** **** {where_from[1][-4:]} -> {where[0]} **{where[1][-4:]}")
+                print(f"{where[0]} {where[-1][:4]} {where[-1][4:6]}** **** {where[-1][-4:]} -> {where_from[0]} **{where_from[-1][-4:]}")
 
         except KeyError as e:
-            print(f"{where_from[0]} {where_from[1][:4]} {where_from[1][4:6]}** **** {where_from[1][-4:]}")
+            print(f"{where[0]} {where[-1][:4]} {where[-1][4:6]}** **** {where[-1][-4:]}")
         print(display["operationAmount"]["amount"], display["operationAmount"]["currency"]["name"])
 
         print()
-
